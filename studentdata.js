99808e6c9887f4d4727b860cabcc2b8c28fdeb9e
Danny944 <fadehandaniel2006@gmail.com>
2023-09-02 01:20:15 +0100

    Students Objects

---
+++ studentdata.js
@@ -0,0 +1,27 @@
+export const studentName = [
+  {
+    name: "David",
+    matNo: 20121,
+    id: 1,
+  },
+  {
+    name: "Daniel",
+    matNo: 20122,
+    id: 2,
+  },
+  {
+    name: "Damilare",
+    matNo: 20123,
+    id: 3,
+  },
+  {
+    name: "Lade",
+    matNo: 20124,
+    id: 4,
+  },
+  {
+    name: "Mide",
+    matNo: 20125,
+    id: 5,
+  },
+];
